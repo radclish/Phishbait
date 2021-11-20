ec8996d7c1f364fc7d6c5360f454a5da7b5bcd53
eylne2716 <93164823+eylne2716@users.noreply.github.com>
2021-11-20 18:34:47 -0500

    Updated to Authentication
    Updating the registration and sign-in process to properly update and pull from the database.

--- authentication.php
+++ authentication.php
@@ -10,13 +10,25 @@
         $password = mysqli_real_escape_string($con, $password);  
       
         $sql = "select *from login where email = '$email'";
+		$userid = "select userid from login where email = '$email'";
+		$FirstName = "select FirstName from names where userid = '$userid'";
 		
         $result = mysqli_query($con, $sql);  
         $row = mysqli_fetch_array($result);
 		$current_password = $row['password'];
           
         if (password_verify($password, $current_password)) {  
-            echo "<h1><center> Login successful </center></h1>";  
+			$userid = "select userid from login where email = '$email'";
+			$uidresult = mysqli_query($con, $userid);  
+        	$uidrow = mysqli_fetch_array($uidresult);
+			$uid = $uidrow['userid'];
+			
+			$FirstName = "select FirstName from names where userid = '$uid'";
+			$fnresult = mysqli_query($con, $FirstName);  
+        	$fnrow = mysqli_fetch_array($fnresult);
+			$fn = $fnrow['FirstName'];
+            echo "<h1><center>Welcome, </center></h1>", $fn;
+			header("Location: https://www.phishbait.org/loginsuccessful.html");
         }  
         else{  
             echo "<h1> Login failed. Invalid email or password.</h1>";  
